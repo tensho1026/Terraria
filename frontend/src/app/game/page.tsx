ff004587d28608fe26890d4ccc08d7911fe7272c
tensho1026 <tensho1026@icloud.com>
2025-09-30 02:16:55 +0900

    animation使ってみた

--- frontend/src/app/game/page.tsx
+++ frontend/src/app/game/page.tsx
@@ -1,9 +1,29 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+
+type Place = {
+  x: number;
+  y: number;
+};
 
 function Page() {
-  const [playerPosition, setPlayerPosition] = useState({ x: 0, y: 0 });
+  const [playerPosition, setPlayerPosition] = useState<Place>({ x: 0, y: 0 });
+  const moveX = useRef(0);
 
+  useEffect(() => {
+    const moveLoop = () => {
+      setPlayerPosition((p) => {
+        if (moveX.current <= 400) {
+          moveX.current += 1;
+          return { ...p, x: moveX.current };
+        } else {
+          return p;
+        }
+      });
+      if (moveX.current <= 400) requestAnimationFrame(moveLoop);
+    };
+    moveLoop();
+  }, []);
   useEffect(() => {
     const handleKeyDown = (e: KeyboardEvent) => {
       setPlayerPosition((p) => {
@@ -19,13 +39,27 @@ function Page() {
       window.removeEventListener("keydown", handleKeyDown);
     };
   }, []);
+
+  useEffect(() => {
+    const handleKeyUp = (e: KeyboardEvent) => {
+      if (e.key === "ArrowRight") {
+        console.log("KeyUp");
+      }
+    };
+    window.addEventListener("keyup", handleKeyUp);
+    return () => {
+      window.removeEventListener("keyup", handleKeyUp);
+    };
+  }, []);
+
   return (
     <div>
       <p
         className="absolute top-0 left-0"
         style={{
           transform: `translate(${playerPosition.x}px, ${playerPosition.y}px)`,
-        }}>
+        }}
+      >
         ⭕️
       </p>
       <div className="absolute top-40 left-40"></div>
