e9eecf5d0cd9cc4fd6018e6a40a12f10e4fa146f
tensho1026 <tensho1026@icloud.com>
2025-09-29 22:41:27 +0900

    プレイヤーした移動だけ実装

--- frontend/src/app/game/page.tsx
+++ frontend/src/app/game/page.tsx
@@ -1,9 +1,25 @@
+"use client";
+import { useState } from "react";
+
 function Page() {
+  const [playerPosition, setPlayerPosition] = useState({ x: 0, y: 0 });
+  const handleDown = () => {
+    setPlayerPosition({ x: playerPosition.x, y: playerPosition.y + 10 });
+  };
   return (
     <div>
-      <h1>Game</h1>
+      <p
+        className="absolute top-0 left-0"
+        style={{
+          transform: `translate(${playerPosition.x}px, ${playerPosition.y}px)`,
+        }}>
+        ⭕️
+      </p>
+      <div className="absolute top-40 left-40">
+        <button onClick={handleDown}>↓</button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Page
+export default Page;
