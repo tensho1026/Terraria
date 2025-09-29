c56e389fc0f0e3854fec54516646694b1ec07f92
tensho1026 <tensho1026@icloud.com>
2025-09-30 01:15:29 +0900

    feat:keyboard play

--- frontend/src/app/game/page.tsx
+++ frontend/src/app/game/page.tsx
@@ -1,11 +1,24 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function Page() {
   const [playerPosition, setPlayerPosition] = useState({ x: 0, y: 0 });
-  const handleDown = () => {
-    setPlayerPosition({ x: playerPosition.x, y: playerPosition.y + 10 });
-  };
+
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      setPlayerPosition((p) => {
+        if (e.key === "ArrowRight") return { ...p, x: p.x + 10 };
+        if (e.key === "ArrowLeft") return { ...p, x: p.x - 10 };
+        if (e.key === "ArrowDown") return { ...p, y: p.y + 10 };
+        if (e.key === "ArrowUp") return { ...p, y: p.y - 10 };
+        return p;
+      });
+    };
+    window.addEventListener("keydown", handleKeyDown);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
   return (
     <div>
       <p
@@ -15,9 +28,7 @@ function Page() {
         }}>
         ⭕️
       </p>
-      <div className="absolute top-40 left-40">
-        <button onClick={handleDown}>↓</button>
-      </div>
+      <div className="absolute top-40 left-40"></div>
     </div>
   );
 }
